53242e46c12f83fe51496ab948d8d03e3f3b88e8
Przemek <okonprzemek@gmail.com>
2018-08-30 15:15:04 +0200

    xd

--- importvm/listvm.ps1
+++ importvm/listvm.ps1
@@ -1,14 +1,8 @@
-﻿#$Username = 'ttit\listvm'
-#$Password = ''
-#$pass = ConvertTo-SecureString -AsPlainText $Password -Force
-#$SecureString = $pass
-# Users you password securly
-#$MySecureCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username,$SecureString
+﻿# Skrypt pobierający i zapisujący do pliku HTML listę maszyn wirtualnych z serwerów Hyper-V.
 
-#---
-
-$import_pass = Get-Content C:\skrypt\importvm\pass2.txt | ConvertTo-SecureString
-$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'ttit\listvm',$import_pass
+#Import credentiali do konta które pobierze VMki
+$import_pass = Get-Content C:\test\pass.txt | ConvertTo-SecureString
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'domain\user',$import_pass
 
 $Header = @"
 <style>
@@ -19,9 +13,11 @@ TD {border-width: 1px; padding: 3px; border-style: solid; border-color: black;}
 </style>
 "@
 $global:i=0
-$waw = { import-module VirtualMachineManager; get-vm -VMMServer gungnir.tt.com.pl  |  select @{Name="Item#";Expression={$global:i++;$global:i}},Name,VirtualMachineState,Hostname,OperatingSystem,IsHighlyAvailable,IsRecoveryVM,IsPrimaryVM}
-$lista = Invoke-Command -ComputerName gungnir.tt.com.pl -ScriptBlock $waw -Credential $cred
-$lista | ConvertTo-Html -Head $Header > C:\listavm\waw.html
+#import z pierwszego serwera
+$waw = { import-module VirtualMachineManager; get-vm -VMMServer nazwa_serwera  |  select @{Name="Item#";Expression={$global:i++;$global:i}},Name,VirtualMachineState,Hostname,OperatingSystem,IsHighlyAvailable,IsRecoveryVM,IsPrimaryVM}
+$lista = Invoke-Command -ComputerName nazwa_serwera -ScriptBlock $waw -Credential $cred
+$lista | ConvertTo-Html -Head $Header > C:\listavm\plik1.html
 import-module VirtualMachineManager
-$ldz = get-vm -VMMServer vmm2012r2.tt.com.pl | select @{Name="Item#";Expression={$global:i++;$global:i}},Name,VirtualMachineState,Hostname,OperatingSystem,IsHighlyAvailable,IsRecoveryVM,IsPrimaryVM
-$ldz | ConvertTo-Html -Head $Header >  C:\listavm\ldz.html
+#import z drugiego serwera
+$ldz = get-vm -VMMServer nazwa_serwera | select @{Name="Item#";Expression={$global:i++;$global:i}},Name,VirtualMachineState,Hostname,OperatingSystem,IsHighlyAvailable,IsRecoveryVM,IsPrimaryVM
+$ldz | ConvertTo-Html -Head $Header >  C:\listavm\plik2.html
